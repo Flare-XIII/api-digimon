3e7c6748e7c5634a3b057384450dcf4112ef5ae8
Rogers Casanova <casanovarogers@gmail.com>
2023-09-12 13:06:22 -0300

    Primera versión con algunas funcionalidades como el navbar

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const URL_BASE = "https://www.digi-api.com/api/v1/digimon/"
+
+let searchDigimon = document.getElementById('inputDigimon')
+let buttonDigimon = document.getElementById('searchDigimon')
