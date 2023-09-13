d6d2fcdfb613be7a1f100dcb3bdeed49bace00cb
Rogers Casanova <casanovarogers@gmail.com>
2023-09-13 18:53:14 -0300

    Fin de día 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,53 @@
 const URL_BASE = "https://digimon-api.vercel.app/api/digimon"
 
 let searchDigimon = document.getElementById('inputDigimon')
-let buttonDigimon = document.getElementById('searchDigimon')
-let card = document.getElementById('card')
+let buttonDigimon = document.getElementById('searchButton')
+let card = document.getElementById('cardSuccess')
+
+function showCard (digimon) {
+    let digimonCard = `
+    <div class="card" style="width: 18rem;">
+        <img src="${digimon.img}" class="card-img-top" alt="./assets/img/noDigimon.png">
+        <div class="card-body">
+            <h5 class="card-title">${digimon.name}</h5>
+            <p class="card-text">${digimon.level}</p>
+        </div>
+    </div>
+    `
+    cardSuccess.innerHTML = digimonCard
+}
 
 function getOneDigimon () {
     buttonDigimon.addEventListener('click', function (event) {
         event.preventDefault()
         let getDigimon = searchDigimon.value
-        fetch(`${URL_BASE}${getDigimon}`).then(response => response.json()).then(digimon => showCard(digimon))
+        fetch(`${URL_BASE}/name/${getDigimon}`).then(response => response.json()).then(digimon => showCard(digimon[0]))
     })
 }
 
 function getAllDigimon () {
     fetch(URL_BASE).then(response => response.json()).then(digimon => {
-        let digimones = digimon.content
 
         let card = ''
 
-        digimones.forEach(oneDigimon => {
+        digimon.forEach(oneDigimon => {
 
             card += `
+            <div class = "col-xl-3 col-lg-4 mb-4 mb-md-3 col-md-6 col-sm-12"
             <div class="card" style="width: 18rem;">
-                <img src="${oneDigimon.image}" class="card-img-top" alt="./assets/img/noDigimon.png">
-                <div class="card-body">
-                    <h5 class="card-title">${oneDigimon.name}</h5>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
+            <img src="${oneDigimon.img}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${oneDigimon.name}</h5>
+            <button type="button" class="btn btn-primary">Primary</button>
+            </div>
+            </div>
             </div>
             `
 
-            tarjeta.innerHTML = card
+            cardSuccess.innerHTML = card
         })
     })
 } 
 
-function showCard (digimon) {
-
-    let card = `
-    <div class="card" style="width: 18rem;">
-        <img src="${digimon.images[0].href}" class="card-img-top" alt="">
-        <div class="card-body">
-            <h5 class="card-title">${digimon.name}</h5>
-            <p class="card-text">${digimon.descriptions[1].description}</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-    </div>
-    `
-    tarjeta.innerHTML = card
-}
-
 getAllDigimon()
 getOneDigimon()
-
-const darkMode = document.getElementById('darkMode');
-
-darkMode.addEventListener ('click', function () {
-
-    const body = document.body;
-
-    if (body.style.backgroundColor === black) {
-        body.style.backgroundColor = 'rgba(0, 3, 36, 0.684)';
-    } else {
-        body.style.backgroundColor = 'black';
-    }
-});
